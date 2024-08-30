031f774d63b24d423ac1aa7cbda63c203d6025a4
Jose Tomas Cortina Espitia <joseto.corties@gmail.com>
2024-08-29 21:31:27 -0500

    cuarto commit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,7 +4,7 @@ export const GET_GADGETS = "GET_GADGETS";
 
 export const getGadgets = () => async (dispatch) => {
     try {
-        const response = await axios.get('http://localhost:3001/gadgets');
+        const response = await axios.get('https://gadgetsphere-server-production.up.railway.app/gadgets');
         dispatch({ type: GET_GADGETS, payload: response.data });
     } catch (error) {
         console.log(error);
