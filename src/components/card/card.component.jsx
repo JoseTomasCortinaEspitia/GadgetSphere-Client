99459acf8d54fdf664e0b22987497599cdd5d7cd
Jose Tomas Cortina Espitia <joseto.corties@gmail.com>
2024-09-10 20:42:39 -0500

    montando card

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,25 @@ import styles from './card.module.css'
 const Card = ({gadget}) => {
     //console.log(gadget)
     return (
-        
+        <>
+        <div className="card h-100">
+                          {/* Product image */}
+                          <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                          {/* Product details */}
+                          <div className="card-body p-4">
+                              <div className="text-center">
+                                  {/* Product name */}
+                                  <h5 className="fw-bolder">Fancy Product</h5>
+                                  {/* Product price*/}
+                                  $40.00 - $80.00
+                              </div>
+                          </div>
+                          {/* <!-- Product actions--> */}
+                          <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                              <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                          </div>
+                      </div>
+
         <div className={styles.card} >
             <div className={styles.card__content}>
                 <p>{gadget.name}</p>
@@ -12,6 +30,7 @@ const Card = ({gadget}) => {
                 <p>Cantidad: {gadget.stock}</p>
             </div>
         </div>
+        </>
     )
 }
 
