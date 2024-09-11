f14fbca695b07d944014e49fba703b2e6f309882
Jose Tomas Cortina Espitia <joseto.corties@gmail.com>
2024-09-11 10:41:59 -0500

    card4

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,16 +5,51 @@ const Card = ({gadget}) => {
     //console.log(gadget)
     return (
         <>              
-                  
+        
+            <div className="col mb-5">
+                <div className="card h-100">
+                        {/* <!-- Sale badge--> */}
+                        <div className="badge bg-dark text-white position-absolute" style={{ top: '0.5rem', right: '0.5rem' }}>Sale</div>
+                        {/* <!-- Product image--> */}
+                        <img className="card-img-top" src={gadget.image} alt="..." />
+                        {/* <!-- Product details--> */}
+                        <div className="card-body p-4">
+                            <div className="text-center">
+                                {/* <!-- Product name--> */}
+                                <h5 className="fw-bolder">{gadget.name}</h5>
+                                {/* <!-- Product reviews--> */}
+                                <div className="d-flex justify-content-center small text-warning mb-2">
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                </div>
+                                {/* <!-- Product price--> */}
+                                <span className="text-muted text-decoration-line-through">$20.00</span>
+                                ${gadget.price}COP
+                            </div>
+                        </div>
+                        {/* <!-- Product actions--> */}
+                        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent" style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+                            <div className="text-center">
+                                <a className="btn btn-outline-dark mt-auto" href="#">Ver mas</a>
+                            </div>
+                            <div className="text-center">
+                                <a className="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a>
+                            </div>
+                        </div>  
+                </div>
+            </div>
 
-        <div className={styles.card} >
+        {/* <div className={styles.card} >
             <div className={styles.card__content}>
                 <p>{gadget.name}</p>
                 <p>Precio: {gadget.price}</p>
                 <img className={styles.imgcard} src={gadget.image} alt="" />
                 <p>Cantidad: {gadget.stock}</p>
             </div>
-        </div> 
+        </div>  */}
         </>
     )
 }
--- src/components/cards/cards.component.jsx
+++ src/components/cards/cards.component.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Card from "../card/card.component"
 
 import styles from './cards.module.css'
@@ -5,6 +6,20 @@ import styles from './cards.module.css'
 const Cards = ({allGadgets}) => {
     //console.log(allGadgets)
     return (
+        <>
+        {/* <!-- Header--> */}
+        <header className="bg-dark py-5">
+          <div className="container px-4 px-lg-5 my-5">
+              <div className="text-center text-white">
+                  <h1 className="display-4 fw-bolder">Compra con estilo</h1>
+                  <p className="lead fw-normal text-white-50 mb-0">Hacemos la vida de tu mascota más fácil y divertida</p>
+              </div>
+          </div>
+      </header>
+        {/* // <!-- Section--> */}
+        <section className="py-5">
+          <div className="container px-4 px-lg-5 mt-5">
+            <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"></div>
         <div className={styles.cards}>
             {
                 allGadgets.map((gadget) => (
@@ -12,6 +27,9 @@ const Cards = ({allGadgets}) => {
                 ))
             }           
         </div>
+        </div>
+        </section>
+        </>
     )
 }
 
